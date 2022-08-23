360787585dc4f15fd42e829adf8f802ba76be098
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-08-23 17:28:15 +0545

    transports

---
+++ week2/ex_3_3.py
@@ -0,0 +1,4 @@
+transports = ["honda", "Astin martin", "bently"]
+print("i would like to owne a " + transports[0])
+print("i wise i have " + transports[1])
+print("i will definately have a " + transports[3] + " one day")
