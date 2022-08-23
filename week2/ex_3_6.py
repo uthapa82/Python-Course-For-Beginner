f49e1ca8ba849a2b1c118e4a3449b63007276c4c
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-08-23 19:37:15 +0545

    moreguest

---
+++ week2/ex_3_6.py
@@ -0,0 +1,6 @@
+guests = ["Anu", "Upendra", "Anup"]
+print("I found bigger dinner table")
+guests.insert (0, "Abhishek")
+guests.insert (2, "Abhijeet")
+guests.append ("abhi")
+print("Welcome everyone for dinner party.")
