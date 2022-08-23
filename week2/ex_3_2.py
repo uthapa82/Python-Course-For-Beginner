bd8c48914ea5cdfd27bde00b3bdc37acbe01692b
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-08-23 17:25:52 +0545

    message

---
+++ week2/ex_3_2.py
@@ -0,0 +1,5 @@
+names = ['Anup', 'Abhi', 'Anu', 'Anusha']
+print("Hello, How are you " + names[0])
+print("I've something for you " + names[1])
+print("Namaste " + names[2] + " dede")
+print("how are you doing " + names[3] + " sis")
