a85763daf4f648d70426fff55983a9648fe60961
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-08-24 16:23:39 +0545

    lesson

---
+++ week2/ex_3_10.py
@@ -0,0 +1,15 @@
+fruits = ["Appe", "Mango", "Banana"]
+fruits [0] = "orange" #modifying apple with orange
+print(fruits)
+
+fruits.append("pears") #append adds element at the end of the list
+print(fruits)
+
+fruits.insert(2, "grpes") #insert help to insert anything in the in list 
+print(fruits)
+
+fruits.remove("pears") #.remove helps to remover mentioned elemts from the list
+print(fruits)
+
+fruits.pop(-1) #.pop will remove only last elements from the list
+print(fruits)
