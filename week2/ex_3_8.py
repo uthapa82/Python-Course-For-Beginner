170950d9ac77eb62709955612e9101e37ff43ba4
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-08-24 15:37:58 +0545

    confusing1

---
+++ week2/ex_3_8.py
@@ -0,0 +1,25 @@
+places = ["santorini", "pokhara", "taj mahal", "grand canyon", "mount Fuji"]
+print(places)
+places.sort()
+print(sorted (places))
+print(places)
+
+places.reverse()
+print(sorted(places, reverse = True))
+
+places.reverse()
+print(places)
+
+places.reverse()
+print(places)
+
+places.sort()
+print(sorted (places))
+
+places.sort()
+print(sorted (places))
+
+
+
+
+ 
