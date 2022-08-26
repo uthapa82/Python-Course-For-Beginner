474152fad77191f8668174ea7b3ee7fb96cb552a
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-08-26 22:12:48 +0545

    alyen

---
+++ week2/ex_5_4.py
@@ -0,0 +1,13 @@
+#if
+alyen_color = 'green'
+if alyen_color == 'green':
+    print("You just earn 5points.")
+else:
+    print("you just earn 10 points.")
+
+#else
+alyen_color = 'yelllow'
+if alyen_color == 'green':
+    print("you just earned 5 points.")
+else:
+    print("you just earned 10 points")
