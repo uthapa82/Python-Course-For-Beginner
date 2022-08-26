50a68e6f72f5e05541c98984db9e11ae97c38fb7
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-08-26 21:08:30 +0545

    cinfitional

---
+++ week2/ex_5_1.py
@@ -0,0 +1,25 @@
+bike = 'Honda'
+print("is bike == 'Honda'? I predict True.")
+print(bike == 'Honda')
+
+print("\nIs bike == 'audi'? I predict False")
+print(bike == 'audi')
+
+fruit = 'Apple'
+print("Is fruit == 'Apple'? Ipredict True.")
+print(fruit == 'Apple')
+
+print("\nIS fruit == 'Hpnda'? I predict False.")
+print(fruit == 'Honda')
+
+phone = 'samsung'
+print("Is phone == 'samsung'? I predict True.")
+print(phone == 'samsung')
+
+print("\nIS phone == 'Tesla'? I predict False.")
+print(phone == 'Tesla')
+
+mammel = 'Human'
+print("IS mammel == 'Human'? I predict True")
+print(mammel == 'Human')
+
