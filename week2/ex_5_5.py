277a526bd647db79ad559031fced472848e5b5b2
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-08-27 22:27:31 +0545

    if_elif_else

---
+++ week2/ex_5_5.py
@@ -0,0 +1,12 @@
+alien_color = ["green", "yellow", "red"]
+if alien_color == "green":
+     print("The player earned 5 points")
+     
+elif alien_color == "yellow":
+    print("Th player earned 10 points")
+
+else:
+    print("The player earned 15 points")
+
+
+ 
