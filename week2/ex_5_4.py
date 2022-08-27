fb1d1e9272fd302641b52d77f437f93754ebe47d
uthapa82 <tupendra022@gmail.com>
2022-08-27 09:46:33 -0400

    correction done

--- week2/ex_5_4.py
+++ week2/ex_5_4.py
@@ -1,13 +1,13 @@
 #if
-alyen_color = 'green'
-if alyen_color == 'green':
-    print("You just earn 5points.")
+alien_color = 'green'
+if alien_color == 'green':
+    print("You just earn 5 points.")
 else:
     print("you just earn 10 points.")
 
 #else
-alyen_color = 'yelllow'
-if alyen_color == 'green':
+alien_color = 'yelllow'
+if alien_color == 'green':
     print("you just earned 5 points.")
 else:
     print("you just earned 10 points")
