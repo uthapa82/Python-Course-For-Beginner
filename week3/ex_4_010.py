9edae39a47e6bd55db0fce05b88788eceaf8b6df
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-08-30 20:19:16 +0545

    animals

---
+++ week3/ex_4_010.py
@@ -0,0 +1,9 @@
+animals = ["dog", "cat", "cow", "buffalo", "bull", "monkey"]
+print("A dog would make a great pet")
+print(animals[0:3])
+
+print("three items from the middle of the list are animals")
+print(animals[1:4])
+
+print("the last three items in the list are")
+print(animals[-3: ])
