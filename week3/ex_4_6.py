b3f9a1ef6355e30aef89752a8826787a59e3925e
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-08-31 20:23:42 +0545

    oddnumbers

---
+++ week3/ex_4_6.py
@@ -0,0 +1,4 @@
+"""Use third argument of the range() to make a list of odd numbers from 1 to 20. use for loop to print each no"""
+odd_numbers = list(range(1,21,2))#third argument is 2
+for odd_numbers in range(1,21,2):
+    print(odd_numbers)
