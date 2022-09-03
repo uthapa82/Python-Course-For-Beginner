f188515a7781c74a3c02fec67d78980efffbd6bf
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-03 22:18:47 +0545

    week3

---
+++ week3/exercise.py
@@ -0,0 +1,12 @@
+numbers = list[range(1,15)] # i.e range goes from 1 to 14 only.
+for numbers in range(1,15):
+    print(numbers)
+
+number = range(1,50)
+print(max(number))   #finding max numbers in a list
+
+print(min(number))  #finding minimum numbers.
+
+cubes = [value**3 for value in range(5,11)]
+print(cubes)
+
