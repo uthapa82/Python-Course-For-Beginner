3a72a0b4908cc43f315799c1378e3d269876baa8
uthapa82 <tupendra022@gmail.com>
2022-09-03 22:42:06 -0400

    ex4.5 correction

--- week3/ex_4_5.py
+++ week3/ex_4_5.py
@@ -1,8 +1,11 @@
-numbers = range(1,100001)
+numbers = list(range(1,100001))
 
+#new_list = [x for x in range(1, 100001)]
+#new_list = list(range(1, 100))
 print(min(numbers))
 
 print(max(numbers))
 
 print(sum(numbers))
 
+print(numbers)
