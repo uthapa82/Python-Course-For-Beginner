ed2e5ad14158e06f1fa97dcb545d656084c382eb
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-04 22:51:10 +0545

    admin

--- week4/ex_5_8.py
+++ week4/ex_5_8.py
@@ -5,6 +5,7 @@ for i in usernames:
 print("hello " + usernames[1] + " would you like to see a status report?")
 print("hello " + usernames[2] + " thank you for logging in again")
 
+#another one
 username = ["Anu", "admin", "eric", "anu", "ram"]
 for i in username:
-    print("Hello " + i  + "would you like to see a status report?") 
+    print("Hello " + i  + " would you like to see a status report?") 
