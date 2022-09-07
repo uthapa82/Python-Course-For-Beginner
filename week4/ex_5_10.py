307e9d86fc02c751ecd2a405d2282feb7acc26e8
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-07 22:46:59 +0545

    users

---
+++ week4/ex_5_10.py
@@ -0,0 +1,12 @@
+current_users = ["Ram", "shyam", "om", "Hari", "Gita"]
+new_users = ["Abhi", "Ram", "Abhijeet", "Shyam", "Anu"]
+
+for i in new_users:
+    print(i)
+    
+    if new_users == "Ram":
+        print("you need to enter a new user name")
+
+    else:
+        print("the user name is available ")
+
