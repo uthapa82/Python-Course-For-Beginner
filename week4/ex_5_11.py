3dc1fd5539a2fbcf4a18a83d8af6839f361eda7d
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-08 21:08:18 +0545

    done

---
+++ week4/ex_5_11.py
@@ -0,0 +1,12 @@
+ordinal_no = ['1', '2', '3', '4', '5', '6', '7', '8', '9',]
+for i in ordinal_no:
+    print(i)
+    if i == '1':
+        print("1st")
+    elif i == '2':
+        print("2nd")
+    elif i == '3':
+        print("3rd")
+    else:
+        print(i + "th")
+    
---
+++ week4/ex_6_1.py
@@ -0,0 +1,11 @@
+#using dictonary.
+person_name = {
+    "first_name": "Abhishek", 
+    "last_name": "Lamichhane",
+    "age": "19", 
+    "city": "kathmandu"
+    }
+print(person_name["first_name"])
+print(person_name["last_name"])
+print(person_name["age"] + "yrs old")
+print(person_name["city"] + " city")
