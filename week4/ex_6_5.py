d5aed6c8d2a63d8db943f558555b359cbee581c5
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-09 16:34:32 +0545

    test2

---
+++ week4/ex_6_5.py
@@ -0,0 +1,14 @@
+rivers = {
+    "Bishnumati": "Machhapokhari",
+    "Hang Ho": "China",
+    "Nile": "Egypt"
+}
+
+for k, v in rivers.items():
+    print("\nk:" + k + " river " + "runs through " + v + ".")
+
+for key, value in rivers.items():
+    print("\nkey:" + key + " river.")
+
+for key, value in rivers.items():
+    print("\nvalue:" + value)
