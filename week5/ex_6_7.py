cc1a6385a0afdc8f82cfa0c9a3dab693b436931e
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-11 19:23:52 +0545

    test

---
+++ week5/ex_6_7.py
@@ -0,0 +1,27 @@
+people_list_1 = {
+    'first_name': 'Abhishek', 
+    'last_name': 'Lamichhane',
+    'age': '19', 
+    'city': 'kathmandu',
+    }
+people_list_2 = {
+    'first_name': 'anu',
+    'last_name': 'thapa',
+    'age': 22,
+    'city': 'bhaktapur',
+}
+
+people_list_3 = {
+    'firat_name': 'abhi',
+    'last_name': 'guragai',
+    'age': 16,
+    'city': 'kathmandu',
+}
+
+people_list = [people_list_1, people_list_2, people_list_3]
+
+for people in people_list:
+    print(people)
+
+    
+    
