59b037fa5b8365a2b369c113ef040f4310fce43f
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-12 16:37:54 +0545

    reviewed

--- week4/ex_6_3.py
+++ week4/ex_6_3.py
@@ -1,12 +1,12 @@
 #Glossary.
 # correction == ''
 prog_words = {
-    "append": "to add something",
-    "pop": "to remove last element",
-    "insert": "to insert some element anywhere ina list",
-    "del": "to delete",
-    "remove": "to remove elements from list"}
+    'append': 'to add something',
+    'pop': 'to remove last element',
+    'insert': 'to insert some element anywhere ina list',
+    'del': 'to delete',
+    'remove': 'to remove elements from list',}
 
 for k, v in prog_words.items():
-    print(k + ": " + v)
+    print(k + ": " + v + ".")
             
