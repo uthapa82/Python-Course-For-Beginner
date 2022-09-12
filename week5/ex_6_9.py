e190f919395e5819ed5bb4236cb386fe85e4cb10
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-12 23:16:20 +0545

    favorite_place

---
+++ week5/ex_6_9.py
@@ -0,0 +1,9 @@
+favorite_place = {
+    'Anu': 'pokhara',
+    'Anup': ['bhaktapur', 'kathmandu'],
+    'Anuhsa': ['mustang', 'illam', 'chitwan'],
+}
+#favorite_place['Anu'] = "lakesite"
+for names, places in favorite_place.items():
+    print(names + " favorite place are: ")
+    print(places)
