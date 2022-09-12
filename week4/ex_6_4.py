d40da715d6db22d9637a00abe3813baf8519f070
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-12 16:42:42 +0545

    reviewed

--- week4/ex_6_4.py
+++ week4/ex_6_4.py
@@ -1,9 +1,10 @@
 prog_words = {
-    ".append": "to add something",
-    ".pop": "to remove last element",
-    ".insert": "to insert some element anywhere ina list",
-    "del": "to delete",
-    ".remove": "to remove elements from list"}
+    'append': 'to add something',
+    'pop': 'to remove last element',
+    'insert': 'to insert some element anywhere ina list',
+    'del': 'to delete',
+    'remove': 'to remove elements from list',}
+
 
 for k, v in prog_words.items():
     # TODO
@@ -12,17 +13,16 @@ for k, v in prog_words.items():
 
 #add more pythin terms to your glossary.
 more_pwords = {
-    ".append": "to add something",
-    ".pop": "to remove last element",
-    ".insert": "to insert some element anywhere ina list",
-    "del": "to delete",
-    ".remove": "to remove elements from list",
-    ".sort": "to sort something",
-    ".reverse": "to reverse something",
-    ".lower()": "to change element into lowercase",
-    ".upper()": "change element into uppercase"}
+    '.append': 'to add something',
+    '.pop': 'to remove last element',
+    '.insert': 'to insert some element anywhere ina list',
+    'del': 'to delete',
+    '.remove': 'to remove elements from list',
+    '.sort': 'to sort something',
+    '.reverse': 'to reverse something',
+    '.lower()': 'to change element into lowercase',
+    '.upper()': 'change element into uppercase'}
     
 for k, v in more_pwords.items():
     # TODO
-    print("\nk:" + k + " meaning is")
-    print("v:" + v)
+    print(k + " meaning is " + v + ".")
