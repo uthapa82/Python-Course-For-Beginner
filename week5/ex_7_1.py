bd5153827c9e5b80adcd678e2605249f1d0019a9
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-14 17:20:06 +0545

    function.

---
+++ week5/ex_7_1.py
@@ -0,0 +1,6 @@
+#New chapter FUNCTION()..
+def rental_car(user):
+    """Display a simple greeting."""
+    print("Hello," + user.title() + "'Let me see if i can find you a Subaru!'")
+
+rental_car('abhi')
