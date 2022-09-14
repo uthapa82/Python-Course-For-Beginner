05fc9633140a1d61b7a17e8c608d4e89bc00cbac
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-14 16:53:19 +0545

    extension

---
+++ week5/ex_6_12.py
@@ -0,0 +1,8 @@
+favorite_place = {
+    'Anu': 'pokhara',
+    'Anup': ['bhaktapur', 'kathmandu'],
+    'Anuhsa': 'mustang',}
+
+favorite_place['abhi'] = 'banepa'
+print(favorite_place)
+print("Anu like to visit " + favorite_place['Anu'] + ".")
