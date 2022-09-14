4ec3f683232d83cba1e7face3c501a871720b4f3
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-14 23:09:18 +0545

    resturant seating

---
+++ week5/ex_7_2.py
@@ -0,0 +1,10 @@
+user = input("How many people are there in your dinner group?")
+
+if user >= '8':
+        print("They'll have to wait for a dinner")
+else:
+    print("The table is ready")
+        
+
+
+
