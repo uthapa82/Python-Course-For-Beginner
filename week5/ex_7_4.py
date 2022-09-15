b1114719b23a4096f27576f92b13212c7003b179
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-15 16:45:49 +0545

    pizza topping

---
+++ week5/ex_7_4.py
@@ -0,0 +1,19 @@
+#Practice.
+#prompt = ("tell me who your are")
+#prompt = ("what is your name." )
+#name = input(prompt)
+#print("hello" + name)
+
+#Pizza topping.
+prompt = ("\nenter a series of pizza topping")
+prompt += ("\n Enter 'quit' to end the program" )
+#message = " " #"you'll add topping to their pizza"
+
+while True:#message != 'quit':
+    message = input(prompt)
+   
+    if message == 'quit':
+         break
+
+    else:
+        print("you'll add topping to your pizza")
