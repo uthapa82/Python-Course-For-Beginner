98f4dfb335dfdf6f4f1bb5d5eb7320414ad02b75
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-15 17:22:42 +0545

    test

--- week5/ex_7_1.py
+++ week5/ex_7_1.py
@@ -1,6 +1,12 @@
 #New chapter FUNCTION()..
 def rental_car(user):
     """Display a simple greeting."""
-    print("Hello," + user.title() + "'Let me see if i can find you a Subaru!'")
+    print("Hello," + user.title() + " 'Let me see if i can find you a Subaru!'")
 
 rental_car('abhi')
+
+def user_name(ram):
+    """diaplay a simple greetings"""
+    print("hello," + ram.title() + " where are you from")
+
+user_name('abhishek')
