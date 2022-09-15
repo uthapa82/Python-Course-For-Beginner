7fa11810609366c1a55482855ad96b80da7aad5d
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-15 17:18:57 +0545

    movie ticket

---
+++ week5/ex_7_5.py
@@ -0,0 +1,16 @@
+prompt = ("Enter your age")
+prompt += ("\n Enter 'q' to end the program")
+
+while True:
+    age = input(prompt)
+   
+    if age == 'quit':
+        break
+
+    if age < 3:
+        print("ticket is free")
+        
+    elif age < 12:
+        print("the ticket is $10")
+    else:
+        print("the ticket is $15")
