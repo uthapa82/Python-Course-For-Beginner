be263cfc5d16b0d760f21d1cc57786bdcca9ca8c
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-17 20:55:14 +0545

    reviewed

--- week5/ex_6_11.py
+++ week5/ex_6_11.py
@@ -1,17 +1,25 @@
-cities_0 = {
-    'kathmandu':  {'country': 'it is inNepal', 
-                 'population': ' with 20M popu.', 
-                 'fact': 'fact is fact',},
-    'NewYok': {'country': 'is in US', 
-               'population': ' ith 30Mpopu.', 
-               'fact': 'not in US',},
-    'pokhara': {'country': 'is in Nepal', 
-                'population': ' with 40M popu.',
-                'fact': 'fact',}
+cities = {
+    'kathmandu':  {
+        'country': 'Nepal',
+        'population': 200000,
+        'fact': 'Mt.Everest',
+        },
+    'NewYok': {
+        'country': 'US',
+        'population': 300000,
+        'fact': 'white house',
+        },
+    'pokhara': {
+        'country': 'Nepal',
+        'population': 400000,
+        'fact': 'lake',
+        }
 }  #print the name of each city and also all information.
-for city, info in cities_0.items():
+for city, info in cities.items():
     print("\n" + city.title() + " City:")
-    information = info['country'] + info['population'] + "\n" + info['fact']
-    print(information)
+    #information = info['country'] + "\n" + str(info['population']) + "\n" + info['fact']
+    #print(information)
+    print(" It has population of " + str(info['population']))
+    print(" There are many " + info['fact'])
 
     
