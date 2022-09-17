7e2bdc8674099949f495e707d8055f5a1a6b0567
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-17 19:56:02 +0545

    reviewed

--- week5/ex_6_7.py
+++ week5/ex_6_7.py
@@ -1,27 +1,35 @@
+main_list = []
 people_list_1 = {
     'first_name': 'Abhishek', 
     'last_name': 'Lamichhane',
     'age': '19', 
     'city': 'kathmandu',
     }
+main_list.append(people_list_1)
+
 people_list_2 = {
     'first_name': 'anu',
     'last_name': 'thapa',
     'age': 22,
     'city': 'bhaktapur',
 }
+main_list.append(people_list_2)
 
 people_list_3 = {
-    'firat_name': 'abhi',
+    'first_name': 'abhi',
     'last_name': 'guragai',
     'age': 16,
     'city': 'kathmandu',
 }
-
+main_list.append(people_list_3)
 people_list = [people_list_1, people_list_2, people_list_3]
 
-for people in people_list:
-    print(people)
+for people in main_list:
+    full_name = people['first_name'].title() + " " + people['last_name'].title()
+    people_age = people['age']
+    people_city = people['city'].title()
+    print(full_name + " is " + str(people_age) + " years old " +
+     " lives in " + people_city) 
 
     
     
