f1581b3068c39efe1be059f99645347846c27f56
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-18 08:35:38 +0545

    large shirt

---
+++ week5/ex_8_4.py
@@ -0,0 +1,8 @@
+def make_shirt(size, shirt_0 = 'medium', shirt = 'large',):
+    """display information about t-shirt"""
+    print("i love python " + shirt)
+    print("The shirt is " + shirt)
+    print("The shirt is " + shirt_0)
+    print("The size of the shirt is " + str(size))
+
+make_shirt(size = 20,)
