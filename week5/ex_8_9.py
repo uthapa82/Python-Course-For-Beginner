9cb96bd56ef5cc17581cea37811b7dc545fe024d
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-18 15:41:50 +0545

    magicians

---
+++ week5/ex_8_9.py
@@ -0,0 +1,7 @@
+def magicians (names):
+    """Display magicians name"""
+    for name in names:
+        megician = "hello, " + name.title() + "."
+        print(megician)
+username = ["Abhi", "Anup", "Anup"]
+magicians(username)
