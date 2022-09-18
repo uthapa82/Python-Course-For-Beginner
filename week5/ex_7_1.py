40e964903954a2027c32f1285627a0acf8c7d839
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-18 07:15:30 +0545

    test5

--- week5/ex_7_1.py
+++ week5/ex_7_1.py
@@ -3,10 +3,17 @@ def rental_car(user):
     """Display a simple greeting."""
     print("Hello," + user.title() + " 'Let me see if i can find you a Subaru!'")
 
-rental_car('abhi')
+#rental_car('abhi')
 
 def user_name(ram):
     """diaplay a simple greetings"""
     print("hello," + ram.title() + " where are you from")
 
-user_name('abhishek')
+#user_name('abhishek')
+
+def main():
+    rental_car('abhi')
+    user_name('abhishek')
+
+if __name__ == "__main__":
+    main()
