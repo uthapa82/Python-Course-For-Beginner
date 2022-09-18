aaa7f46f0f51987238b02b924a4e7f2fe72d0a91
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-18 22:41:21 +0545

    review

--- week5/ex_8_2.py
+++ week5/ex_8_2.py
@@ -2,7 +2,6 @@ def favorite_book(book):
     """Display a simple greeting."""
     print("One of my favorite books is " + book.title() + "!")
 
-favorite_book("harry potter")
 def main():
   favorite_book("harry potter")
 
