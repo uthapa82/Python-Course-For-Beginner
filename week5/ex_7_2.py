13db986c150409a7cf73669d8565b00531ffc862
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-18 07:15:44 +0545

    test6

--- week5/ex_7_2.py
+++ week5/ex_7_2.py
@@ -1,6 +1,6 @@
 user = input("How many people are there in your dinner group?")
 user = int(user)
-if user >= 8: #str(8)
+if user > 8: #str(8)
         print("They'll have to wait for a dinner")
 else:
     print("The table is ready")
