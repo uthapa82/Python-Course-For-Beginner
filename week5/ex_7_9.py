cecc0071cfcb5787a8c0f4ae6760a06bc16949b3
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-18 07:16:42 +0545

    test11

--- week5/ex_7_9.py
+++ week5/ex_7_9.py
@@ -1,8 +1,17 @@
 #No pastrami.
-print("The deli has run out of pastrami.")
-sandwich_orders = ["veg sandwich", "tuna sandwich", "pastrami sandwich"]
+sandwich_orders = ["veg sandwich", "tuna sandwich", "pastrami sandwich", "pastrami sandwich"]
+finished_sandwich = []
 
+print("The deli has run out of pastrami.")
 while "pastrami sandwich" in sandwich_orders:
     sandwich_orders.remove("pastrami sandwich")
-print(sandwich_orders)
+
+while sandwich_orders:
+    current_order = sandwich_orders.pop()
+    print("verifying orders: " + current_order)
+    finished_sandwich.append(current_order)
     
+print("\n")
+print("The following sandwich have been moved to finished sandwich list:")
+for j in finished_sandwich:
+    print(j.title() + " was made!")   
