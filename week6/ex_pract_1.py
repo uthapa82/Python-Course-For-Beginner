2b54f74d2830980b4285388b93088c62011589a9
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-23 21:09:17 +0545

    test

--- week6/ex_pract_1.py
+++ week6/ex_pract_1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def foo_bar(n):
     for i in range(1, n):
         if i %3 == 0 and i %5 == 0:
@@ -11,7 +10,7 @@ def foo_bar(n):
             print("bar")
             
 foo_bar(n=13)
-=======
+
 def foo_bar(int_n):
     for num in range(1, int_n):
         if num % 3 == 0:
@@ -24,4 +23,4 @@ def foo_bar(int_n):
             print(num)
 
 foo_bar(int_n = 16)
->>>>>>> 926d130a7f77bc4a71eb0539d026be395361b8b1
+
