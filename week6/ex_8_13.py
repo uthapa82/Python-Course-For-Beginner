754114fca841f2c98e69080eae19c34a91820642
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-25 08:46:44 +0545

    user profile

---
+++ week6/ex_8_13.py
@@ -0,0 +1,16 @@
+def build_profile(first, last, **user_info):
+    profile = {}
+    profile['first_nmae'] = first.title()
+    profile['last_name'] = last.title()
+    for key, value in user_info.items():
+        profile[key] = value
+    return profile
+
+user_profile = build_profile('abhishek', 'lamichhane',
+age = 19, address = 'kharibot', profile = 'student')
+
+def main():
+    print(user_profile)
+
+if __name__ == '__main__':
+    main()
