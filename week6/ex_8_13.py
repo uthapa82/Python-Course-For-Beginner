e5f618e445dae653469b4941d1b839e053a5fd62
uthapa82 <tupendra022@gmail.com>
2022-09-25 08:41:03 -0400

    reveiwed

--- week6/ex_8_13.py
+++ week6/ex_8_13.py
@@ -1,15 +1,12 @@
 def build_profile(first, last, **user_info):
-    profile = {}
-    profile['first_nmae'] = first.title()
-    profile['last_name'] = last.title()
-    for key, value in user_info.items():
-        profile[key] = value
-    return profile
+    user_info['first_name'] = first.title()
+    user_info['last_name'] = last.title()
 
-user_profile = build_profile('abhishek', 'lamichhane',
-age = 19, address = 'kharibot', profile = 'student')
+    return user_info
+
 
 def main():
+    user_profile = build_profile('abhishek', 'lamichhane', age = 19, address = 'kharibot', profile = 'student')
     print(user_profile)
 
 if __name__ == '__main__':
--- week6/ex_8_14.py
+++ week6/ex_8_14.py
@@ -1,16 +1,19 @@
-from pyexpat import model
-
-
-def build_car(manufacture, model_name, **car_info):
-    car = {}
-    car['car_name'] = manufacture
-    car['model'] = model_name
-
+def build_car(manufacture, model, **car_info):
+    car = {
+        'car name': manufacture.title(),
+        'model': model.title()
+    }
+    
     for key, value in car_info.items():
         car[key] = value
+        
     return car
 
-car_info = build_car('subaru', 'out track', color = 'blue', two_package = True)
+def main():
+    car_info = build_car('subaru', 'out track', color = 'blue', two_package = True)
+    car_info2 = build_car('honda', 'accord', year=2014, color='blue', headlights='led')
+    print(car_info)
+    print(car_info2)
 
-print(car_info)
-
+if __name__ == '__main__':
+    main()
