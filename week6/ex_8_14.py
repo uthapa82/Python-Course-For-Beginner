0ee0591b6f07561f42da05077ba644d05cae827c
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-26 22:53:49 +0545

    build_car

--- week6/ex_8_14.py
+++ week6/ex_8_14.py
@@ -1,16 +1,20 @@
 from pyexpat import model
 
 
-def build_car(manufacture, model_name, **car_info):
-    car = {}
-    car['car_name'] = manufacture
-    car['model'] = model_name
+def build_car(manufacture, model, **car_info):
+    car = {
+        'car name': manufacture.title(),
+        'model': model.title()
+    }
 
     for key, value in car_info.items():
         car[key] = value
     return car
 
-car_info = build_car('subaru', 'out track', color = 'blue', two_package = True)
 
-print(car_info)
+def main():
+    car_info= build_car('subaru', 'out track', color = 'blue', two_package = True)
+    print(car_info)
 
+if __name__ == '__main__':
+    main()
