d056693984922dedce24370ce1f6548adfae87e9
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-26 22:38:08 +0545

    test

--- week6/ex_8_13.py
+++ week6/ex_8_13.py
@@ -1,6 +1,6 @@
 def build_profile(first, last, **user_info):
     profile = {}
-    profile['first_nmae'] = first.title()
+    profile['first_name'] = first.title()
     profile['last_name'] = last.title()
     for key, value in user_info.items():
         profile[key] = value
