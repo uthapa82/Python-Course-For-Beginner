723c22867efbb9e4bddbf77570b2d5318118cccb
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-09-27 16:48:11 +0545

    User

---
+++ week7/ex_9_3.py
@@ -0,0 +1,21 @@
+class User():
+    def __init__(self, first_name, last_name, age, profession):
+        self.first_name = first_name.title()
+        self.last_name = last_name.title()
+        self.age = age 
+        self.profession = profession.title()
+    
+    def describe_user(self):
+        print(self.first_name + " " + self.last_name + " is a " + self.profession)
+
+    def greet_user(self):
+        print("Hello " + self.first_name + " " + self.last_name)
+
+user = User('abhi', 'lamichhane', 15, 'student')
+user1 = User('anup', 'thapa', 22, 'business man')
+
+user.describe_user()
+user1.describe_user()
+user1.greet_user()
+    
+    
