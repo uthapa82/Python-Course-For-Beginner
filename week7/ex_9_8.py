bd90cc15fe71c86c957443956415f931d7e7e205
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-10-03 21:24:37 +0545

    test

--- week7/ex_9_8.py
+++ week7/ex_9_8.py
@@ -42,8 +42,10 @@ user_0 = Admin('Elon', 'Musk', 36, 'CEO')
 user_0.describe_user()
 
 print("\n" + "privileges:")
-user_0.privileges = [
+user_0_privileges = [
     "can add post", 
     "can delete post", 
     "can ban user"
 ]   
+user_0.privileges.privileges = user_0_privileges
+user_0.privileges.show_priviliges()
