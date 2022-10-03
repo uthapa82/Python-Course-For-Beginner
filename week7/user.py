b60768b2e277fa15c915af4e59f7a477d06f8af6
Abhishek Lamichhane <lamichhaneabhi10@gmail.com>
2022-10-03 21:26:10 +0545

    test1

---
+++ week7/user.py
@@ -0,0 +1,21 @@
+#ex_9_12
+class User():
+    def __init__(self, first_name, last_name, age, profession):
+        self.first_name = first_name.title()
+        self.last_name = last_name.title()
+        self.age = age 
+        self.profession = profession.title()
+        self.login_attempts = 0
+
+    def increment_login_attempts(self):
+        print("you have login " + str(self.login_attempts) + " times")
+        self.login_attempts += 1
+        
+    def reset_login_attempts(self):
+        self.login_attempts == 0
+    
+    def describe_user(self):
+        print(self.first_name + " " + self.last_name + " is a " + self.profession)
+
+    def greet_user(self):
+        print("Hello " + self.first_name + " " + self.last_name)
