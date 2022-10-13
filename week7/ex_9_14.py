c0d451f40be13f8d8b63bd58e04347904dbea2f4
labhi <lamichhaneabhi1@gmain.com>
2022-10-13 23:32:16 +0545

    dice

---
+++ week7/ex_9_14.py
@@ -0,0 +1,16 @@
+from random import randint
+
+class Dice():
+    """Display information about Rolling dice!"""
+    def __init__(self, sides = 6):
+        self.sides = sides
+
+    def roll_dice(self):
+        randint(1, self.sides)
+#for rolling dice 10times..
+    for i in range(1, 11):
+        roll_dice
+
+#for rolling dice 20 times..
+    for j in range(1, 21):
+        roll_dice
