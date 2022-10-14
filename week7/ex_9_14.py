da5bcb4ac026bca938b7b53f800d73d058a15ed6
uthapa82 <tupendra022@gmail.com>
2022-10-13 21:51:02 -0400

    reveiwed

--- week7/ex_9_14.py
+++ week7/ex_9_14.py
@@ -2,15 +2,45 @@ from random import randint
 
 class Dice():
     """Display information about Rolling dice!"""
-    def __init__(self, sides = 6):
+    def __init__(self, sides=6):
         self.sides = sides
 
     def roll_dice(self):
-        randint(1, self.sides)
-#for rolling dice 10times..
-    for i in range(1, 11):
-        roll_dice
+        return randint(1, self.sides)
 
-#for rolling dice 20 times..
-    for j in range(1, 21):
-        roll_dice
+# six sided dice
+dice_6 = Dice()
+
+print("\nResult of 6 sided Dice")
+result_6 = []
+#rolls dice 10 times..
+for i in range(1, 11):
+    result = dice_6.roll_dice()
+    result_6.append(result)
+
+print(result_6)
+
+# 10 sided dice
+dice_10 = Dice(sides=10)
+
+print("\nResult of 10 sided Dice")
+
+result_10 = []
+#rolls dice 10 times..
+for i in range(1, 11):
+    result = dice_10.roll_dice()
+    result_10.append(result)
+
+print(result_10)
+# 20 sided dice 
+dice_20 = Dice(sides=20)
+
+print("\nResult of 20 sided Dice")
+
+result_20 = []
+#rolls dice 10 times..
+for i in range(1, 11):
+    result = dice_20.roll_dice()
+    result_20.append(result)
+
+print(result_20)
