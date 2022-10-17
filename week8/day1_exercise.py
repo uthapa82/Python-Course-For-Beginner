9f537bfd0c7324b44c050ae14e30d4841891d2de
labhiCS <101660254+labhiCS@users.noreply.github.com>
2022-10-17 21:27:18 +0545

    Update day1_exercise.py

--- week8/day1_exercise.py
+++ week8/day1_exercise.py
@@ -1,21 +1,20 @@
 #------------------------------------------------------------------------------------------
-# Day 1 Exercise                                                                          #
-# 10/17/2022                                                                              #
-#                                                                                         #            
-# 1. Print the following pattern using loop (function name => pattern_print())            #
-#   $ python3 day1_exercise.py                                                            #
-#      1                                                                                  #
-#      2 2                                                                                #
-#      3 3 3                                                                              #
-#      4 4 4                                                                              #
-#                                                                                         #
-# 2. Ask a user for a number store it in a list and find the length of the list           #
-#   function name :- list_length(user_list)                                               #
-#                                                                                         #
-#                                                                                         #
-# 3. Write a function to reverse a user string. function name reverse_string(user_string) #
-#      Smaple: user input:thapa output: apaht                                             #
+# Day 1 Exercise                                                                          
+# 10/17/2022                                                                                                                                                                       #            
+1. Print the following pattern using loop (function name => pattern_print())            
+   $ python3 day1_exercise.py                                                            
+      1                                                                                  
+      2 2                                                                                
+      3 3 3                                                                              
+      4 4 4                                                                              
+                                                                                         
+ 2. Ask a user for a number store it in a list and find the length of the list           
+   function name :- list_length(user_list)                                               
+                                                                                         
+                                                                                         
+ 3. Write a function to reverse a user string. function name reverse_string(user_string) 
+      Smaple: user input:thapa output: apaht                                             
 #-------------------------------------------------------------------------------------------
 
-# REMOVE THE comment below 
-# use the python recommended format def function ... def main() ... if __name__ == '__main__'
+ REMOVE THE comment below 
+ use the python recommended format def function ... def main() ... if __name__ == '__main__'
