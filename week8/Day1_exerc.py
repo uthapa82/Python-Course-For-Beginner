8eb72823b0ecdb4b56f3516e99337dd7fa122f7c
labhiCS <lamichhaneabhi1@gmail.com>
2022-10-18 22:20:16 +0545

    Day1_exe

---
+++ week8/Day1_exerc.py
@@ -0,0 +1,19 @@
+def pattern(n):
+    for i in range(1, n):
+        print(str(i) * i)
+
+def list_length(user_list):
+    print(len.user_list)
+
+def reverse_string(user_string):
+    print(user_string.reverse())
+
+def main():
+    pattern(10)
+    userinput = int(input("Enter the number to put in a list"))
+    user_list = ["abhishek", "abhi", "anusha"]
+    user_string = "Abhishek"
+    #print(user_string.reverse())
+
+if __name__ == "__main__":
+    main()
