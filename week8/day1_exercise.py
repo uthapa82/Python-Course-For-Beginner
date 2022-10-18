755e988a000d1f7ad616832cfbb24fb4f4bc0797
uthapa82 <tupendra022@gmail.com>
2022-10-18 19:11:23 -0400

    solution

--- week8/day1_exercise.py
+++ week8/day1_exercise.py
@@ -15,4 +15,61 @@
 #                                                                                         #
 # 3. Write a function to reverse a user string. function name reverse_string(user_string) #
 #      Sample: user input:thapa output: apaht                                             #
-#-------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------
+
+"""
+__summary__
+program that includes function to print pattern, 
+reverse string and return the length of the string 
+
+"""
+
+def pattern_print(num):
+    """
+    takes a integer input and prints a pattern till num
+    Args:
+        num (_type_): integer
+    """    
+    for i in range(1, num):
+        print(str(i) * i)
+
+def list_length(user_lst):
+    """takes user list and prints the length of the list 
+
+    Args:
+        user_lst (_list_): user list 
+    """    
+    print(len(user_lst))
+
+def reverse_string(user_string):
+    """takes string from user and prints the reverse of it 
+
+    Args:
+        user_string (string): _description_
+    """    
+    print(user_string[::-1])
+
+def main():
+
+    #user input pattern_print
+    print("Print Pattern......")
+    number = int(input("Enter number to print pattern till: "))
+    pattern_print(number)
+
+    #user_list input 
+    ("Length of the list: ")
+    user_input = input("Enter the numbers separated by spaces")
+    # for i in range(0, userinput):
+    #     value = int(input('Enter a number: '))
+    #     user_list.append(value)
+    user_list = user_input.split()
+    list_length(user_list)
+
+    #user string input
+    print("Reverse the string....")
+    user_string = input("Please Enter a Word to reverse: ")
+    reverse_string(user_string)
+
+
+if __name__ =='__main__':
+    main()
