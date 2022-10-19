f78343aa5d2bfdc7230edb7ae8cba296f0697e9d
uthapa82 <tupendra022@gmail.com>
2022-10-19 08:20:01 -0400

    day 2 exercise file

---
+++ week8/day2_exercise.py
@@ -0,0 +1,56 @@
+#------------------------------------------------------------------------------------------
+# Day 2 Exercise                                                                          #
+# 10/19/2022                                                                              #
+#                                                                                         #            
+# 1. Take a list as a user input, first and last number, change the first and last element#
+#  of the list                                                                            #
+#   function name ==> swap_element(user_list, first, last)                                #
+#                                                                                         #
+#                                                                                         #
+# 2. Write a function which takes two numbers and find the maximum and minimum numbers    #
+#     function name ==> max_min(num1, num2)                                               #                                       
+#                                                                                         #
+# 3. Write a function that takes a word as input from user and prints 'the word is even'  #
+#    if the word contains even number of character, else print 'the word is odd           #
+#      funtion name ==>  odd_even(user_string )                                           #
+#                                                                                         #
+# Sample output:                                                                          #
+#    $ python3 day2_exercise.py                                                           #
+#         Exercise 1 ......                                                               #
+#         Please enter number separated by spaces: 0 2 3 4 6                              #
+#         Original List: [0 2 3 4 6]                                                      #
+#         Changed List: [1 2 3 4 5]                                                       #
+#         
+#         Exercise 2...........                                                           #
+#         Maximim number is : 9                                                           #
+#         Minimum number is : 6                                                           #
+#                                                                                         #
+#         Exercise 3 ...........                                                          #
+#         Please enter a word: Anu                                                        #
+#         The word is odd (because Anu is three character 'a', 'n', 'u')                  #
+#------------------------------------------------------------------------------------------
+"""
+    Remove below when you're done with this exercise 
+    
+    for exercise 1 you can take list as a input in a similar way we talked in day1 exercise 1 
+    then just take first and last number from user and change the element in that postion
+    like list_user[0] = what user want to change 
+    
+    for exercise 2 it's just a conditional if and else statement that's it 
+
+    for exercise 3 it's similar to list_length in day1 exercise but you might need to add some
+    conditionals to check odd and even  that's it 
+
+    Good luck and PLEASE TRY TO FOLLOW THE CONVENTIONS WE DISCUSSED and no extra .py files this
+    should be one file that's it 
+    def f...
+
+    def f...
+
+    def ...
+
+    def main():
+        
+    if __name__ == '__main__':
+        main()
+"""
