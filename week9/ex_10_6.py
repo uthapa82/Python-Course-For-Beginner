fbf867f94aa1413e2c7da7622aee740ae3abaad9
labhiCS <lamichhaneabhi1@gmail.com>
2022-11-04 22:29:36 +0545

    Adding

---
+++ week9/ex_10_6.py
@@ -0,0 +1,11 @@
+try:
+    first = int(input("enter first number: "))
+
+    second = int(input("Enter second number: "))
+
+except TypeError:
+    print("sorry, Enter number only")
+
+else:
+    sum = first + second
+    print("The sum is " + str(first) + " and " + str(second) + " is " + str(sum))
