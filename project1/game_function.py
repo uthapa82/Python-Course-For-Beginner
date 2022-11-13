6a3c6a205aa99b37c09f57c272d9f470e40c8300
labhiCS <lamichhaneabhi1@gmail.com>
2022-11-13 22:54:58 +0545

    Update Screen Function

---
+++ project1/game_function.py
@@ -0,0 +1,17 @@
+import sys
+
+import pygame
+def check_events():
+    """Respond to keypresses and mouse events."""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+def update_screen(ai_settings, screen, ship):
+    """Update images on the screen and flip to the new screen."""
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    pygame.display.filp
+
+    
