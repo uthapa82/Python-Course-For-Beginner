f99af7b9591e124c1c52fc7c2b124813d6dccbf8
labhiCS <lamichhaneabhi1@gmail.com>
2022-11-13 07:23:53 +0545

    project1

--- project1/alien_invasionn.py
+++ project1/alien_invasionn.py
@@ -2,12 +2,16 @@ import sys
 
 import pygame
 
+#from ship import Ship
+
 def run_game():
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
 
     bg_color = (0, 0, 230) #creation blue screen only.
+
+    #ship = Ship(screen)
     
     while True:
         for event in pygame.event.get():
@@ -15,6 +19,8 @@ def run_game():
                 sys.exit()
                 
         screen.fill(bg_color)
+
+        #ship.blitme()
 
         pygame.display.flip()
 
---
+++ project1/setting.py
@@ -0,0 +1,6 @@
+class Settings():
+ """A class to store all settings for Alien Invasion."""
+ def __init__(self):
+    self.screen_width = 1200
+    self.screen_height = 800
+    self.bg_color = (0, 0, 230) #crreating blue screen
