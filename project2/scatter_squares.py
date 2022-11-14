a8557d52a01406be7030840819267c14b0a1ecc9
uthapa82 <thapau1@tcnj.edu>
2022-11-13 21:46:57 -0500

    matplotlib

---
+++ project2/scatter_squares.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
+plt.scatter(x_values, y_values, s=200)
+
+# set chart title and label axes 
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.xlabel("Square of Value", fontsize=14)
+
+# set size of tick labels
+plt.tick_params(axis='both', which='major', labelsize=14)
+plt.show
