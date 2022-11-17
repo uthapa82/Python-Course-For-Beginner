3666b1da228b0484b249a1f40e55b75304fff7e5
labhiCS <lamichhaneabhi1@gmail.com>
2022-11-17 16:16:25 +0545

    Molecular motion 15.3

--- project2/rw_visual.py
+++ project2/rw_visual.py
@@ -2,8 +2,20 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    rw = RandomWalk(5000)
+    rw.fill_walk()
 
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    plt.figure(figsize =(20, 12))
+
+    point_number = list(range(rw.num_points))
+    plt.plot(rw.x_values, rw.y_values, linewidth=1)
+
+    plt.scatter(0, 0, c='green', edgecolors='none', s=120)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=115)
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
