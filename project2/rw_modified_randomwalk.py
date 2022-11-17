ac025bb759b87f808b1f921763ba93c5675dd425
labhiCS <lamichhaneabhi1@gmail.com>
2022-11-17 16:42:11 +0545

    ex 15.4 Modiying RandomWalk

---
+++ project2/rw_modified_randomwalk.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+from modified_random_walk import RandomWalk
+
+while True:
+    rw = RandomWalk(5000)
+    rw.fill_walk()
+
+    plt.figure(figsize =(20, 12))
+
+    point_number = list(range(rw.num_points))
+    plt.plot(rw.x_values, rw.y_values, linewidth=1)
+
+    plt.scatter(0, 0, c='green', edgecolors='none', s=120)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=115)
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
