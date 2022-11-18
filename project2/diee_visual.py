5a8ef7f247d3d0933540f1d54c3085ea31aebc55
labhiCS <lamichhaneabhi1@gmail.com>
2022-11-18 21:35:03 +0545

    Modifying file die_visual.py

---
+++ project2/diee_visual.py
@@ -0,0 +1,22 @@
+import pygal as pg
+from die import Die
+
+#Roll one  dice.
+die = Die()
+
+#using list comprehensive.
+results = [die.roll() for roll_num in range(1000)]
+
+frequencies = [results.count(value) for value in range (1, die.num_sides+1)]
+
+
+# visualize the results 
+hist = pg.Bar()
+
+hist._title = "Results of rolling on D6 1000 times."
+hist.x_labels = [str(x) for x in range(1, die.num_sides+1) ]
+hist.x_title = "Results"
+hist._y_title = "Count/Frequency of Results"
+
+hist.add('D6', frequencies)
+hist.render_to_file("diee_visual.svg")
