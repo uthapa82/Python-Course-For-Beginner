73d6443fb04e57b0acbffb12d9988699ef31730f
labhiCS <lamichhaneabhi1@gmail.com>
2022-11-18 22:13:02 +0545

    Two D8s

---
+++ project2/two_dice_visual.py
@@ -0,0 +1,22 @@
+import pygal as pg
+from die import Die
+
+#Rolling two eight slided dice.
+die = Die(8)
+die_2 = Die(8)
+
+result = die.roll() + die_2.roll()
+results = [result for roll_num in range(10000)]
+
+max_result = die.num_sides + die_2.num_sides 
+frequencies = [results.count(value) for value in range(2, max_result+1)]
+
+hist = pg.Bar()
+
+hist._title = "Results of rolling on two d8 10000 times."
+hist.x_labels = [str(x) for x in range(2, max_result+1)]
+hist.x_title = "Results"
+hist._y_title = "Count/Frequency of Results"
+
+hist.add('D8 + D8', frequencies)
+hist.render_to_file("two_dice_visual.svg")
