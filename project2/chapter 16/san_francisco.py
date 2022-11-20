dd7747f2fc9980e03c6dc946532df829f305a8e5
labhiCS <lamichhaneabhi1@gmail.com>
2022-11-20 22:00:20 +0545

    High Low Temperature

--- project2/chapter 16/san_francisco.py
+++ project2/chapter 16/san_francisco.py
@@ -1,39 +1,38 @@
 import csv
-from matplotlib import pyplot as plt
-
+from matplotlib import pyplot as plt 
 from datetime import datetime
 
-filename = '../sitka_weather_2014.csv'
+filename = '../dataset/death_valley_2014.csv'
 
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     
     highs = []
-    dates = []
+    lows = []
     for row in reader:
         try:
-            
-            current_date = datetime.strptime(row[0], "%Y-%m-%d")
-            high = float(row[1])
+            high = int(row[1])
+            low = int(row[3])
             
         except ValueError:
-            print(current_date, 'missing data')
+            print('missing data')
         else:
-            dates.append(current_date)
             highs.append(high)
-            
-
+            lows.append(low)
+    
+   
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(dates, highs, c='red', alpha=0.9)
+plt.plot(highs, c='red', alpha=0.9)
+plt.plot(lows, c='blue', alpha=0.9)
 
-plt.fill_between(dates, highs, facecolor='blue', alpha=0.1)
- 
-plt.title('Daily Rainfall, July 2014', fontsize=24)
+plt.fill_between(highs, lows, facecolor='blue', alpha=0.1)
+
+plt.title('Daily high & low temperatures, July 2014', fontsize=24)
 plt.xlabel('', fontsize=16)
 
 fig.autofmt_xdate()
-plt.ylabel("Rainfall(R)", fontsize=16)
+plt.ylabel("Temperature(F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
 plt.show()    
