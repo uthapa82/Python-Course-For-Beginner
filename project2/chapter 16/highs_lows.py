53e536a16d367c9c8204d7427ff9fc0f30607996
uthapa82 <thapau1@tcnj.edu>
2022-11-19 21:51:55 -0500

    dataset part2

--- project2/chapter 16/highs_lows.py
+++ project2/chapter 16/highs_lows.py
@@ -1,17 +1,45 @@
 import csv
+from matplotlib import pyplot as plt 
+from datetime import datetime
 
 filename = '../dataset/death_valley_2014.csv'
+
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    highs = []
+    dates = []
+    lows = []
+    for row in reader:
+        try:
+            
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+            
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
     
-    temp_high = []
-    for temperature in reader:
-        temp_high.append(temperature[1])
-    
-    highs = [int(i) for i in temp_high]
-    
-    print(highs)
+   
+# plotting part 
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dates, highs, c='red', alpha=0.9)
+plt.plot(dates, lows, c='blue', alpha=0.9)
+
+#shadding
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+# format plot 
+plt.title('Daily high & low temperatures, July 2014', fontsize=24)
+plt.xlabel('', fontsize=16)
+
+fig.autofmt_xdate()
+plt.ylabel("Temperature(F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()    
