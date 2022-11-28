534d407276485128d87c43473d4b5db04d678a0b
labhiCS <lamichhaneabhi1@gmail.com>
2022-11-28 23:15:10 +0545

    Plotting Data

---
+++ project2/chapter 16/plot_dict.py
@@ -0,0 +1,31 @@
+import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
+URL = 'https://api.github.com/search/repositories?q=language:python&sort=star'
+r = requests.get(URL)
+print("Status code:", r.status_code)
+
+response_dict = r.json()
+print("Total repositories:", response_dict['total_count'])
+
+repo_dicts = response_dict['items']
+
+names, plot_dicts = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description'],
+        }
+        
+    plot_dicts.append(plot_dict)
+
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', plot_dict)
+chart.render_to_file('api_visual.svg')
