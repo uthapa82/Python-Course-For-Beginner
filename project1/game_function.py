0859c1bb82aaa1750870fa924da440e97963d1f2
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-18 23:15:51 +0545

    update()

--- project1/game_function.py
+++ project1/game_function.py
@@ -10,7 +10,11 @@ def check_events():
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 #move ship to the right
-                ship.rect.centerx += 1
+                ship.moving_right = True
+                
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
 
 def update_screen(ai_settings, screen, ship):
     """Update images on the screen and flip to the new screen."""
