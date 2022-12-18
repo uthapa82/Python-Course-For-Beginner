1b461238457702e3a9d89b4a444b4ffd8d5b5bf7
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-18 21:29:28 +0545

    run game

--- project1/alien_invasionn.py
+++ project1/alien_invasionn.py
@@ -11,6 +11,7 @@ def run_game():
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
 
+    #For colour background
     bg_color = (230, 230, 230)
 
     ship = Ship(screen)
